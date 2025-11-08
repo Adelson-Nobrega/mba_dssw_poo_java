92dd490c45590c5c30d272077e66ce2a7ee508d6
Adelson-Nobrega <lopes_nobre@yahoo.com.br>
2025-11-08 11:23:54 -0300

    Criação de novas classes do projeto People System.

---
+++ PeopleSystem/src/apps/Application.java
@@ -0,0 +1,19 @@
+package apps;
+
+import auxiliares.Endereco;
+import auxiliares.Telefone;
+import pessoas.Pessoa;
+
+public class Application {
+    static void main() {
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.setNome("João da Silva");
+
+        Telefone tel1 = new Telefone("83", "99999-9999");
+        pessoa1.setTelsContato(tel1);
+
+        Endereco endereco1 = new Endereco("Rua das Flores", "123", "Ap 101", "Centro", "São Paulo", "SP", "11252-000");
+        pessoa1.setEndereco(endereco1);
+    }
+}
--- PeopleSystem/src/pessoas/Pessoa.java
+++ PeopleSystem/src/pessoas/Pessoa.java
@@ -1,14 +1,50 @@
 package pessoas;
 
+import auxiliares.Endereco;
+import auxiliares.Telefone;
+
 import java.time.LocalDate;
 
 public class Pessoa {
     protected String nome;
     protected LocalDate dataNascimento;
-    protected String endereco;
-    protected String telsContato;
+    protected Endereco endereco;
+    protected Telefone telsContato;
 
-    public String obterIdade() {
-        return "idade";
+    public int obterIdade() {
+        LocalDate dataAtual = LocalDate.now();
+        return dataAtual.getYear() - dataNascimento.getYear();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Telefone getTelsContato() {
+        return telsContato;
+    }
+
+    public void setTelsContato(Telefone telefone) {
+        this.telsContato = telefone;
     }
 }
